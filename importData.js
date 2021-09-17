8b301982acc3d0e1135d374f04443b3a613f1121
webersinfotech <53133512+webersinfotech@users.noreply.github.com>
2021-09-17 09:29:19 +0530

    no sandbox

--- importData.js
+++ importData.js
@@ -62,7 +62,7 @@ async function fetchHTML(code) {
     if (cluster.isMaster) {
         console.log(`Primary ${process.pid} is running`);
 
-        const connections = new Array(7).fill(0)
+        const connections = new Array(4).fill(0)
 
         for (let [index, connection] of connections.entries()) {
             await launchCluster()
